e02e9ccd899a745b552d70a44101580ca5549281
qwyng <ikusawasi@gmail.com>
2020-01-05 18:53:37 +0900

    add integrate

---
+++ tests/github-issue.rs
@@ -0,0 +1,17 @@
+#[macro_use]
+extern crate assert_cli;
+
+// TODO mockしたいんだけどどうにもうまくいかない
+#[test]
+fn integration_test() {
+    {
+        assert_cli::Assert::command(&["target/debug/github-issue", "rails", "rails"])
+            .stdout()
+            .contains("number")
+            .stdout()
+            .contains("title")
+            .stdout()
+            .contains("created_at")
+            .unwrap();
+    }
+}
